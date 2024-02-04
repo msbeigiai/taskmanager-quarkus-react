fa84c1192f40a6744225aa5cad1fc7f6fe988623
Mohsen Sadeghbeigi <msbeigi83@gmail.com>
2024-02-04 17:42:54 +0200

    Initial commit

---
+++ src/main/java/org/msbeigi/model/Project.java
@@ -0,0 +1,30 @@
+package org.msbeigi.model;
+
+import io.quarkus.hibernate.reactive.panache.PanacheEntity;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(
+        name = "projects",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"name", "user_id"})
+        }
+)
+public class Project extends PanacheEntity {
+    @Column(nullable = false)
+    public String name;
+
+    @ManyToOne(optional = false)
+    public User user;
+
+    @CreationTimestamp
+    @Column(updatable = false, nullable = false)
+    public ZonedDateTime created;
+
+    @Version
+    public int version;
+
+}
---
+++ src/main/java/org/msbeigi/resources/UserResource.java
@@ -0,0 +1,58 @@
+package org.msbeigi.resources;
+
+import io.smallrye.mutiny.Uni;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import org.jboss.resteasy.reactive.ResponseStatus;
+import org.msbeigi.model.User;
+import org.msbeigi.services.UserService;
+
+import java.util.List;
+
+
+@Path("/api/v1/users")
+public class UserResource {
+
+  @Inject
+  private UserService userService;
+
+  @GET
+  public Uni<List<User>> get() {
+    return userService.list();
+  }
+
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  @ResponseStatus(201)
+  public Uni<User> create(User user) {
+    return userService.create(user);
+  }
+
+  @GET
+  @Path("{id}")
+  public Uni<User> get(@PathParam("id") long id) {
+    return userService.findById(id);
+  }
+
+  @PUT
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Path("{id}")
+  public Uni<User> update(@PathParam("id") long id, User user) {
+    user.id = id;
+    return userService.update(user);
+  }
+
+  @DELETE
+  @Path("{id}")
+  public Uni<Void> delete(@PathParam("id") long id) {
+    return userService.delete(id);
+  }
+
+  @GET
+  @Path("self")
+  public Uni<User> getCurrentUser() {
+    return userService.getCurrentUser();
+  }
+
+}
